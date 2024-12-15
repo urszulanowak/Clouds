9a26c60ecf83f4f5ddf726d8533dc24ee683bfed
urszulanowak <ula.no@onet.pl>
2024-12-15 21:48:45 +0100

    fix

--- transport-management/run.sh
+++ transport-management/run.sh
@@ -2,24 +2,28 @@
 pwd
 ls
 # /opt/render/project/src/
+echo "######################################## 1 ########################################"
 python3 --version
 python3 -m venv venv
 source venv/bin/activate
 python --version
+echo "######################################## 2 ########################################"
 python -m pip install --use-pep517 wheel
 python -m pip install --use-pep517 neo4j
 python -m pip install --use-pep517 Flask
 python -m pip install --use-pep517 flask_cors
-python -m pip install --use-pep517 dotenv
+python -m pip install --use-pep517 python-dotenv
 
-
+echo "######################################## 3 ########################################"
 echo "Starting backend..."
 python transport-management/backend/app.py &
 
+echo "######################################## 4 ########################################"
 cd transport-management
 echo "transport management dir:"
 ls
 pwd
+echo "######################################## 5 ########################################"
 echo "Starting frontend..."
 npm start &
 
