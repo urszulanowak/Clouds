e76ce67a9d977d73e506de23ca78180e5ff4649b
Ula <ula.no@onet.pl>
2024-12-15 20:20:14 +0100

    fix3

--- transport-management/run.sh
+++ transport-management/run.sh
@@ -5,6 +5,8 @@ ls
 python3 --version
 python3 -m venv venv
 source venv/bin/activate
+python3 -m pip install wheel
+python3 -m pip install --upgrade pip setuptools wheel
 -m pip install --upgrade pip
 pip install flask
 python -m pip install -r transport-management/backend/requirements.txt
